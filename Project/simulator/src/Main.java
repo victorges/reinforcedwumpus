6a7eb7c844ea4bb25a253b5fb50c375b44b4bb4a
Victor Elias <victorgelias@gmail.com>
2015-11-13 03:28:57 -0200

    Changing simulator back to printing current score

--- Project/simulator/src/Main.java
+++ Project/simulator/src/Main.java
@@ -2,14 +2,11 @@ public class Main {
     public static void main(String[] args) {
         int iterations = args.length > 0 ? Integer.parseInt(args[0]) : 1;
 
-        int bestScore = Integer.MIN_VALUE;
         for (int i = 0; i < iterations; i++) {
             System.out.println("Running simulation " + i);
             Wumpus game = new Wumpus();
             GameResult result = game.runSimulation();
-
-            if (result.score > bestScore) bestScore = result.score;
-            System.out.println("Finished simulation, best score til now: " + bestScore);
+            System.out.println("Finished simulation, score: " + result.score);
         }
     }
 }
